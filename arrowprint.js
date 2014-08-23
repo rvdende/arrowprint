076669d3512e73b04eeae3a27af29ec4bff662da
Rouan van der Ende <rouan@8bo.org>
2014-08-23 13:14:14 +0200

    bug fixes. now uses argument to specify gcode file

--- arrowprint.js
+++ arrowprint.js
@@ -1,77 +1,54 @@
 // Arrow command line relay
 // this program scans for connected devices and relays communication to a socket server
-// v 5.3
-//ARROW PRINTER
-//READS IN GCODE AND RELAYS TO DUE FOR PRINTING.
-console.log("###########################################")
+// v 5.4beta
 
-//set your gcode file generated from slic3r
-var file = "wade-big.gcode";
 
-console.log("PRINTING: "+file)
-console.log("###########################################")
-///
-var NEWGODE = '{"cmd":"M107"}';
+var file = "";
 
-var TEMP = 0;
+if (!process.argv[2]) {
+	console.log("\n\nERROR. No gcode file.\n Please specify a gcode file to print.")
+	console.log("node arrowprint.js yourpart.gcode\n\n")
+} else {
+	file = process.argv[2];
+}
+
+//var file = "robot_leg_prep.gcode";	//set your gcode file generated from slic3r
+
+var gcodecached = [];
+
+var arduino = {};
 var SerialPort = require("serialport"); //so we can access the serial port
-var os = require("os");
-var TIMElast = os.uptime();
-var STARTTIME = os.uptime();
-
 SerialPort.list( function (err, ports) {
+	console.log("Found devices:")
 	ports.forEach( function(port) {
 		console.log('PORT:'+port.comName);
 		console.log('ID:'+port.pnpId);		
-		console.log(port.manufacturer);
+		console.log(port.manufacturer+"\n");
 
 		if (port.pnpId.indexOf('duino') > 0) {
 			console.log('FOUND ARDUINO')
-			var arduino = new SerialPort.SerialPort('/dev/serial/by-id/'+port.pnpId, {baudrate: 115200});			
-			gcodeprint(arduino);
+			//var arduino = new SerialPort.SerialPort('/dev/serial/by-id/'+port.pnpId, {baudrate: 115200});			
+
 		} else {
 			//console.log('scanning...');					
 		}		
 	});
+
+	arduino = new SerialPort.SerialPort('COM21', {baudrate: 115200});			
+	
 });
 
 
+console.log("\nLoading "+file+" ... please wait\n")
 
-var gcodeprint = function(arduino) {
-	console.log('Connected to Arduino!')
-	var scraper = require('json-scrape')();
-	
-	arduino.on("data", function (data) { 
-		//RAW SERIAL IN
-		//console.log(data.toString());
-		scraper.write(data); 
-	});
-	scraper.on('data', function (obj) {
-		//ARDUINO DATA JSON CONVERTER
-		if (obj.status == "waiting") {
-			//console.log(NEWGODE);
-			arduino.write(NEWGODE); //writes to arduino
-			gcodereader.resume();	//reads another line from file
-		}
-
-		if (obj.thermistor) {
-			//var temperature = (obj.thermistor/Math.pow(2,16))*1024;
-			//temperature = thermistorlookupcelsius(temperature);
-			console.log("Thermistor " + obj.thermistor);
-
-		}
-
-	});
-}
-
-
+var os = require("os");
+var TIMElast = os.uptime();
+var STARTTIME = os.uptime();
 var reader = require ("buffered-reader");
 var BinaryReader = reader.BinaryReader;
 var DataReader = reader.DataReader;
-
 var offset;
 var linecounter = 0;
-
 var gcodereader;
 
 var readerstream = new DataReader (file, { encoding: "utf8" })
@@ -85,8 +62,6 @@ var readerstream = new DataReader (file, { encoding: "utf8" })
 			var text = line.replace(/;.*$/, '').trim(); // Remove comments
 			if (text) {
 				//console.log(text); //RAW GCODE LINE
-				this.pause();		//pause when we find a legit line to print
-				gcodereader = this;
 				var tokens = text.split(' ');
 			    if (tokens) {
 			      var cmd = tokens[0];
@@ -106,104 +81,54 @@ var readerstream = new DataReader (file, { encoding: "utf8" })
 			      TIMElast = os.uptime();
 			      //
 
-			      console.log(linecounter + 'L ' + JSON.stringify(args));
+			      //console.log(linecounter + 'L ' + JSON.stringify(args));
+			      //if (Math.random() < 0.01) { process.stdout.write("."); }
 			      NEWGODE = JSON.stringify(args);
-			      
+			      gcodecached.push(NEWGODE);			      
 			    }
 			}
         })
 
         .on ("end", function (){
-            console.log("==== DONE ====")
+            console.log("end")
             var totaltime = os.uptime() - STARTTIME;
-            console.log(" Total time: " + Math.round(totaltime) + "seconds");
+            console.log(" Total time: " + Math.round(totaltime) + "seconds");            
             console.log(" Total lines: " + linecounter + "of gcode");
             console.log(" Total speed: " + linecounter/totaltime + "lines per second average");
+			console.log(gcodecached.length)
+            //console.log(gcodecached)
+
+            console.log("attempting to print..")
+            gcodeprint(arduino);
         })
         .read ();
 
-
-
-var thermistorlookupcelsius = function(val) {
-	/*
-	Expects a voltage value between 1-1024 and returns Celsius reading.
-	Component: Honeywell 135-104LAG-J01
-	*/
+var gcodeindex = 0;
+var gcodeprint = function(arduino) {
+	console.log('Connected to Arduino!')
+	var scraper = require('json-scrape')();
 	
-	var thermistorlookup = [ 
-	   {"val":1, "celsius": 500},
-	   {"val":46, "celsius": 270}, //top rating 300C
-	   {"val":50, "celsius": 265},
-	   {"val":54, "celsius": 260},
-	   {"val":58, "celsius": 255},
-	   {"val":62, "celsius": 250},
-	   {"val":67, "celsius": 245},
-	   {"val":72, "celsius": 240},
-	   {"val":79, "celsius": 235},
-	   {"val":85, "celsius": 230},
-	   {"val":91, "celsius": 225},
-	   {"val":99, "celsius": 220},
-	   {"val":107, "celsius": 215},
-	   {"val":116, "celsius": 210},
-	   {"val":126, "celsius": 205},
-	   {"val":136, "celsius": 200},
-	   {"val":149, "celsius": 195},
-	   {"val":160, "celsius": 190},
-	   {"val":175, "celsius": 185},
-	   {"val":191, "celsius": 180},
-	   {"val":209, "celsius": 175},
-	   {"val":224, "celsius": 170},
-	   {"val":246, "celsius": 165},
-	   {"val":267, "celsius": 160},
-	   {"val":293, "celsius": 155},
-	   {"val":316, "celsius": 150},
-	   {"val":340, "celsius": 145},
-	   {"val":364, "celsius": 140},
-	   {"val":396, "celsius": 135},
-	   {"val":425, "celsius": 130},
-	   {"val":460, "celsius": 125},
-	   {"val":489, "celsius": 120},
-	   {"val":526, "celsius": 115},
-	   {"val":558, "celsius": 110},
-	   {"val":591, "celsius": 105},
-	   {"val":628, "celsius": 100},
-	   {"val":660, "celsius": 95},
-	   {"val":696, "celsius": 90},
-	   {"val":733, "celsius": 85},
-	   {"val":761, "celsius": 80},
-	   {"val":794, "celsius": 75},
-	   {"val":819, "celsius": 70},
-	   {"val":847, "celsius": 65},
-	   {"val":870, "celsius": 60},
-	   {"val":892, "celsius": 55},
-	   {"val":911, "celsius": 50},
-	   {"val":929, "celsius": 45},
-	   {"val":944, "celsius": 40},
-	   {"val":959, "celsius": 35},
-	   {"val":971, "celsius": 30},
-	   {"val":981, "celsius": 25},
-	   {"val":989, "celsius": 20},
-	   {"val":994, "celsius": 15},
-	   {"val":1001, "celsius": 10},
-	   {"val":1005, "celsius": 5}	
-	]	
-
-	var last = thermistorlookup[0];
-	var cur = thermistorlookup[0];
-	
-	for (var data in thermistorlookup) {
-		cur = thermistorlookup[data];
-		if ((cur.val > val) && (last.val <= val)) {
-			var c = cur.celsius - last.celsius;
-			var v = cur.val - last.val;
-			var vd = val  - last.val;
-			var ratio = vd/v;
-			//console.log(last.celsius+(c*ratio))
-			return last.celsius+(c*ratio);
+	arduino.on("data", function (data) { 
+		//RAW SERIAL IN
+		//console.log(data.toString());
+		scraper.write(data); 
+	});
+	scraper.on('data', function (obj) {
+		//ARDUINO DATA JSON CONVERTER
+		if (obj.status == "waiting") {
+			if (gcodecached.length > gcodeindex) {
+				arduino.write(gcodecached[gcodeindex]); //writes to arduino
+				console.log(gcodecached[gcodeindex]);
+				gcodeindex++;
+			}
 		}
-		last = thermistorlookup[data];
-	}
+
+		if (obj.thermistor) {
+			//var temperature = (obj.thermistor/Math.pow(2,16))*1024;
+			//temperature = thermistorlookupcelsius(temperature);
+			//console.log("Thermistor " + obj.thermistor);
+
+		}
+
+	});
 }
-
-
-
